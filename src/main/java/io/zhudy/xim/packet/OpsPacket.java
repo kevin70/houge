c1887a5d3974799ab4581dd762c45ab4f87ee7e3
kk <kevinz@weghst.com>
2020-06-23 14:40:25 +0800

    添加操作 Packet 接口

---
+++ src/main/java/io/zhudy/xim/packet/OpsPacket.java
@@ -0,0 +1,10 @@
+package io.zhudy.xim.packet;
+
+/**
+ * 操作 {@link Packet}.
+ *
+ * <p>所有实现 {@code OpsPacket} 类型的 Packet 都不需要集群路由.
+ *
+ * @author Kevin Zou (kevinz@weghst.com)
+ */
+public interface OpsPacket extends Packet {}
