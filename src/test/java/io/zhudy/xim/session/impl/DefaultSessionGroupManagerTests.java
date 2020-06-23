6b66ffcbef948aa1f895b2d347019ef8ba0378e1
kk <kevinz@weghst.com>
2020-06-23 10:21:18 +0800

    添加单元测试

--- src/test/java/io/zhudy/xim/session/impl/DefaultSessionGroupManagerTests.java
+++ src/test/java/io/zhudy/xim/session/impl/DefaultSessionGroupManagerTests.java
@@ -32,6 +32,24 @@ class DefaultSessionGroupManagerTests {
   }
 
   @Test
+  void subGroupsRepeat() {
+    var session = new TestSession();
+    var groupIds = Set.of("group1", "group2");
+
+    var dsgm = new DefaultSessionGroupManager();
+    var p = dsgm.subGroups(session, groupIds).then(dsgm.subGroups(session, groupIds));
+    StepVerifier.create(p).verifyComplete();
+
+    // 校验 session 已经添加保存了
+    AsyncCache<String, Set<Session>> groupSessions =
+        Whitebox.getInternalState(dsgm, "groupSessions");
+    for (String groupId : groupIds) {
+      var sessions = groupSessions.synchronous().getIfPresent(groupId);
+      assertThat(sessions).contains(session).hasSize(1);
+    }
+  }
+
+  @Test
   void unsubGroups() {
     var session = new TestSession();
     var groupIds = Set.of("group1", "group2");
@@ -47,5 +65,32 @@ class DefaultSessionGroupManagerTests {
       var sessions = groupSessions.synchronous().getIfPresent(groupId);
       assertThat(sessions).isNull();
     }
+  }
+
+  @Test
+  void unsubGroupsNoSession() {
+    var session = new TestSession();
+    var groupIds = Set.of("group1", "group2");
+
+    var dsgm = new DefaultSessionGroupManager();
+    var p = dsgm.unsubGroups(session, groupIds);
+    StepVerifier.create(p).verifyComplete();
+  }
+
+  @Test
+  void findByGroupId() {
+    var session = new TestSession();
+    var groupIds = Set.of("group1", "group2");
+
+    var dsgm = new DefaultSessionGroupManager();
+    var p = dsgm.subGroups(session, groupIds).thenMany(dsgm.findByGroupId("group1"));
+    StepVerifier.create(p).expectNext(session).verifyComplete();
+  }
+
+  @Test
+  void findByGroupIdNoSession() {
+    var dsgm = new DefaultSessionGroupManager();
+    var p = dsgm.findByGroupId("group1");
+    StepVerifier.create(p).verifyComplete();
   }
 }
