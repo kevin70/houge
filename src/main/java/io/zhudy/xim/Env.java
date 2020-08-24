b26e595de2d0e59bd2f18c3f5f1eaaa6bf4434ed
kk <kevinz@weghst.com>
2020-08-24 16:22:17 +0800

    优化实现逻辑

--- src/main/java/io/zhudy/xim/Env.java
+++ src/main/java/io/zhudy/xim/Env.java
@@ -33,16 +33,22 @@ public enum Env {
   ;
 
   /** 当前的应用环境. */
-  private final static Env C = getEnv();
+  private static final Env C = getEnv();
+
+  /** 系统环境变量名称. */
+  public static final String SYSTEM_ENV_VAR_NAME = "XIM_ENV";
+
+  /** Java 系统属性名称. */
+  public static final String SYSTEM_PROP_VAR_NAME = "xim.env";
 
   /**
    * 当前运行的环境. 默认为 {@link #PROD}.
    *
-   * <p>可通过下列方式设置 xim 的运行环境. <b>按序号查找，忽略大小写</b>.
+   * <p>可通过下列方式设置 xim 的运行环境. <b>按顺序查找，忽略大小写</b>.
    *
    * <ul>
-   *   <li>>通过系统环境变量 {@code XIM_ENV} 设置
-   *   <li>通过 java 启动时命令行参数 {@code -Dxim.env=test} 设置
+   *   <li>设置系统环境变量 {@code XIM_ENV}
+   *   <li>设置 java 启动时命令行参数 {@code -Dxim.env=test} 设置
    * </ul>
    */
   public static Env current() {
@@ -50,10 +56,10 @@ public enum Env {
   }
 
   static Env getEnv() {
-    var k = "XIM_ENV";
+    var k = SYSTEM_ENV_VAR_NAME;
     var v = System.getenv(k);
     if (v == null) {
-      k = "xim.env";
+      k = SYSTEM_PROP_VAR_NAME;
       v = System.getProperty(k);
     }
     if (v == null) {
