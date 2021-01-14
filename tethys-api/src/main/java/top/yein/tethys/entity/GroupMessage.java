3c8d28d080684546696cd99221dd1216adfdf5a6
kk70 <kevinz@weghst.com>
2021-01-14 17:29:02 +0800

    新增群组消息实体类

---
+++ tethys-api/src/main/java/top/yein/tethys/entity/GroupMessage.java
@@ -0,0 +1,32 @@
+package top.yein.tethys.entity;
+
+import java.time.LocalDateTime;
+import lombok.Data;
+
+/**
+ * t_group_message 群组消息.
+ *
+ * @author KK (kzou227@qq.com)
+ */
+@Data
+public class GroupMessage {
+
+  /** 消息 ID 全局唯一. */
+  private String id;
+  /** 群组 ID. */
+  private Long gid;
+  /** 发送人 ID. */
+  private String senderId;
+  /** 消息类型. */
+  private int kind;
+  /** 消息内容. */
+  private String content;
+  /** 统一资源定位器. */
+  private String url;
+  /** 自定义参数. */
+  private String customArgs;
+  /** 创建时间. */
+  private LocalDateTime createTime;
+  /** 修改时间. */
+  private LocalDateTime updateTime;
+}
