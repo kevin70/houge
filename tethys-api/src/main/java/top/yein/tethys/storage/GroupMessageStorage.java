81db5d3855b6505b5dca5d38dce5e07380d97f46
kk70 <kevinz@weghst.com>
2021-01-14 18:04:01 +0800

    新增群组存储接口

--- tethys-api/src/main/java/top/yein/tethys/storage/GroupMessageStorage.java
+++ tethys-api/src/main/java/top/yein/tethys/storage/GroupMessageStorage.java
@@ -1,8 +1,29 @@
 package top.yein.tethys.storage;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import top.yein.tethys.entity.GroupMessage;
+
 /**
  * 群组消息存储.
  *
  * @author KK (kzou227@qq.com)
  */
-public interface GroupMessageStorage {}
+public interface GroupMessageStorage {
+
+  /**
+   * 存储群组消息.
+   *
+   * @param entity 数据实体
+   * @return RS
+   */
+  Mono<Void> store(GroupMessage entity);
+
+  /**
+   * 根据消息 ID 查询群组信息.
+   *
+   * @param id 消息 ID
+   * @return 群组信息
+   */
+  Flux<GroupMessage> findById(String id);
+}
