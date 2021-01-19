7b182270675ddf253cfbfd90e907dae030cc6ee6
kk70 <kevinz@weghst.com>
2021-01-19 14:16:18 +0800

    新增测试工具类

---
+++ tethys-storage/src/test/java/top/yein/tethys/storage/TestUtils.java
@@ -0,0 +1,20 @@
+package top.yein.tethys.storage;
+
+import top.yein.tethys.util.YeinGid;
+
+/**
+ * 测试工具包.
+ *
+ * @author KK (kzou227@qq.com)
+ */
+final class TestUtils {
+
+  /**
+   * 返回消息 ID.
+   *
+   * @return 消息 ID
+   */
+  static String newMessageId() {
+    return new YeinGid(0).toHexString();
+  }
+}
