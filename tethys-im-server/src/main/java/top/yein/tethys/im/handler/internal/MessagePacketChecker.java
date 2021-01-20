11ee7f2854936e928ad8f6836d55f4ae6079ffe9
kk70 <kevinz@weghst.com>
2021-01-20 17:06:26 +0800

    新增消息包校验器

---
+++ tethys-im-server/src/main/java/top/yein/tethys/im/handler/internal/MessagePacketChecker.java
@@ -0,0 +1,67 @@
+package top.yein.tethys.im.handler.internal;
+
+import top.yein.chaos.biz.BizCodeException;
+import top.yein.tethys.constants.MessageKind;
+import top.yein.tethys.core.BizCodes;
+import top.yein.tethys.packet.GroupMessagePacket;
+import top.yein.tethys.packet.PrivateMessagePacket;
+
+/**
+ * 消息校验器.
+ *
+ * @author KK (kzou227@qq.com)
+ */
+public class MessagePacketChecker {
+
+  public static final int MSG_ID_LENGTH = 14;
+
+  /**
+   * 私聊消息校验.
+   *
+   * @param packet 私聊消息
+   */
+  public static void check(PrivateMessagePacket packet) {
+    checkMsgId(packet.getMsgId());
+    checkTo(packet.getTo());
+    checkKind(packet.getKind());
+    checkContent(packet.getContent());
+  }
+
+  /**
+   * 群组消息校验.
+   *
+   * @param packet 群组消息
+   */
+  public static void check(GroupMessagePacket packet) {
+    checkMsgId(packet.getMsgId());
+    checkTo(packet.getTo());
+    checkKind(packet.getKind());
+    checkContent(packet.getContent());
+  }
+
+  static void checkMsgId(String msgId) {
+    if (msgId == null || msgId.length() != 14) {
+      throw new BizCodeException(BizCodes.C3600, "[msgId]不能为空且必须是一个长度为14的字符串");
+    }
+  }
+
+  static void checkTo(String to) {
+    if (to == null || to.isEmpty()) {
+      throw new BizCodeException(BizCodes.C3600, "[to]不能为空");
+    }
+  }
+
+  static void checkKind(int kind) {
+    try {
+      MessageKind.forCode(kind);
+    } catch (IllegalArgumentException e) {
+      throw new BizCodeException(BizCodes.C3600, "[kind]值不合法");
+    }
+  }
+
+  static void checkContent(String content) {
+    if (content == null || content.isEmpty()) {
+      throw new BizCodeException(BizCodes.C3600, "[content]值能为空");
+    }
+  }
+}
---
+++ tethys-im-server/src/test/java/top/yein/tethys/im/handler/internal/MessagePacketCheckerTest.java
@@ -0,0 +1,82 @@
+package top.yein.tethys.im.handler.internal;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.Test;
+import top.yein.chaos.biz.BizCodeException;
+import top.yein.tethys.constants.MessageKind;
+import top.yein.tethys.core.BizCodes;
+import top.yein.tethys.packet.GroupMessagePacket;
+import top.yein.tethys.packet.PrivateMessagePacket;
+import top.yein.tethys.util.YeinGid;
+
+/**
+ * {@link MessagePacketChecker} 单元测试.
+ *
+ * @author KK (kzou227@qq.com)
+ */
+class MessagePacketCheckerTest {
+
+  @Test
+  void checkPrivateMessagePacket() {
+    var packet = new PrivateMessagePacket();
+    packet.setMsgId(new YeinGid(0).toHexString());
+    packet.setFrom("zs");
+    packet.setTo("ls");
+    packet.setKind(MessageKind.TEXT.getCode());
+    packet.setContent("hello world");
+    packet.setUrl("https://tethys.yein.top");
+    packet.setCustomArgs("{}");
+    MessagePacketChecker.check(packet);
+  }
+
+  @Test
+  void checkGroupMessagePacket() {
+    var packet = new GroupMessagePacket();
+    packet.setMsgId(new YeinGid(0).toHexString());
+    packet.setFrom("zs");
+    packet.setTo("ls");
+    packet.setKind(MessageKind.TEXT.getCode());
+    packet.setContent("hello world");
+    packet.setUrl("https://tethys.yein.top");
+    packet.setCustomArgs("{}");
+    MessagePacketChecker.check(packet);
+  }
+
+  @Test
+  void checkMsgId() {
+    assertThatExceptionOfType(BizCodeException.class)
+        .isThrownBy(() -> MessagePacketChecker.checkMsgId(null))
+        .matches((ex) -> ex.getBizCode() == BizCodes.C3600);
+    assertThatExceptionOfType(BizCodeException.class)
+        .isThrownBy(() -> MessagePacketChecker.checkMsgId(""))
+        .matches((ex) -> ex.getBizCode() == BizCodes.C3600);
+  }
+
+  @Test
+  void checkTo() {
+    assertThatExceptionOfType(BizCodeException.class)
+        .isThrownBy(() -> MessagePacketChecker.checkTo(null))
+        .matches((ex) -> ex.getBizCode() == BizCodes.C3600);
+    assertThatExceptionOfType(BizCodeException.class)
+        .isThrownBy(() -> MessagePacketChecker.checkTo(""))
+        .matches((ex) -> ex.getBizCode() == BizCodes.C3600);
+  }
+
+  @Test
+  void checkKind() {
+    assertThatExceptionOfType(BizCodeException.class)
+        .isThrownBy(() -> MessagePacketChecker.checkKind(-1))
+        .matches((ex) -> ex.getBizCode() == BizCodes.C3600);
+  }
+
+  @Test
+  void checkContent() {
+    assertThatExceptionOfType(BizCodeException.class)
+        .isThrownBy(() -> MessagePacketChecker.checkContent(null))
+        .matches((ex) -> ex.getBizCode() == BizCodes.C3600);
+    assertThatExceptionOfType(BizCodeException.class)
+        .isThrownBy(() -> MessagePacketChecker.checkContent(""))
+        .matches((ex) -> ex.getBizCode() == BizCodes.C3600);
+  }
+}
