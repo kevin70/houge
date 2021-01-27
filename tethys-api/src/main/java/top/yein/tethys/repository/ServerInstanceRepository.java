f3a0c1d7c49aa4bcdbd8fa235b84e688697c7bdb
kk70 <kevinz@weghst.com>
2021-01-27 15:18:15 +0800

    新增注释

--- tethys-api/src/main/java/top/yein/tethys/repository/ServerInstanceRepository.java
+++ tethys-api/src/main/java/top/yein/tethys/repository/ServerInstanceRepository.java
@@ -3,14 +3,42 @@ package top.yein.tethys.repository;
 import reactor.core.publisher.Mono;
 import top.yein.tethys.entity.ServerInstance;
 
-/** @author KK (kzou227@qq.com) */
+/**
+ * 服务实例数据访问对象.
+ *
+ * @author KK (kzou227@qq.com)
+ */
 public interface ServerInstanceRepository {
 
+  /**
+   * 新增应用服务实例.
+   *
+   * @param entity 实体
+   * @return 受影响行数
+   */
   Mono<Integer> insert(ServerInstance entity);
 
+  /**
+   * 更新应用服务实例.
+   *
+   * @param entity 实体
+   * @return 受影响行数
+   */
   Mono<Integer> update(ServerInstance entity);
 
+  /**
+   * 更新最后检查时间.
+   *
+   * @param id 应用 ID
+   * @return 受影响行数
+   */
   Mono<Integer> updateCheckTime(int id);
 
+  /**
+   * 查询应用服务实例.
+   *
+   * @param id 应用 ID
+   * @return 实体
+   */
   Mono<ServerInstance> findById(int id);
 }
