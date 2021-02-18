392d3c01f49c88b3982ddf83e8b8c34926fed2a9
kk70 <kevinz@weghst.com>
2021-02-18 14:35:03 +0800

    完善

--- tethys-service/src/main/java/top/yein/tethys/service/PrivateMessageServiceImpl.java
+++ tethys-service/src/main/java/top/yein/tethys/service/PrivateMessageServiceImpl.java
@@ -1,5 +1,8 @@
 package top.yein.tethys.service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import lombok.extern.log4j.Log4j2;
 import reactor.core.publisher.Flux;
 import top.yein.tethys.entity.PrivateMessage;
 import top.yein.tethys.query.PrivateMessageQuery;
@@ -10,7 +13,15 @@ import top.yein.tethys.repository.PrivateMessageRepository;
  *
  * @author KK (kzou227@qq.com)
  */
+@Log4j2
 public class PrivateMessageServiceImpl implements PrivateMessageService {
+
+  /**
+   * 私人消息查询时间限制.
+   *
+   * <p>仅可查询72小时内的消息.
+   */
+  private static final int FIND_MESSAGE_TIME_LIMIT = 72;
 
   private final PrivateMessageRepository privateMessageRepository;
 
@@ -30,6 +41,19 @@ public class PrivateMessageServiceImpl implements PrivateMessageService {
    */
   @Override
   public Flux<PrivateMessage> find(PrivateMessageQuery query) {
+    var createTime = query.getCreateTime();
+    var now = LocalDateTime.now();
+    var d = Duration.between(createTime, now);
+    if (d.toHours() > FIND_MESSAGE_TIME_LIMIT) {
+      var newTime = now.minusHours(FIND_MESSAGE_TIME_LIMIT);
+      log.warn(
+          "查询[{}]私人消息时间[{}]已超过{}小时，将查询时间重置为[{}]",
+          query.getReceiverId(),
+          createTime,
+          FIND_MESSAGE_TIME_LIMIT,
+          newTime);
+      query.setCreateTime(newTime);
+    }
     return privateMessageRepository.find(query);
   }
 }
