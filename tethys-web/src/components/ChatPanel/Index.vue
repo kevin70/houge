65f278689ac251882534a8db1bfbed8856aa8b7a
kk70 <kevinz@weghst.com>
2021-02-25 17:11:59 +0800

    新增 ChatPanel 组件布局

---
+++ tethys-web/src/components/ChatPanel/Index.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { defineProps, reactive } from "vue";
+import ChatHeader from "./_ChatHeader.vue";
+
+const name = "kk70";
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col mt-12">
+    <div class="md:container md:mx-auto">
+      <chat-header />
+      <div class="flex flex-row pt-2 gap-x-2">
+        <div class="w-64 border">
+          <!-- 当前用户的信息 -->
+          <div class="flex flex-row">
+            <div
+              class="rounded-full h-12 w-12 flex items-center justify-center bg-green-500 cursor-pointer"
+            >
+              <span class="m-0.5 truncate">A 7894564</span>
+            </div>
+            <!-- 添加私人或群组会话 -->
+            <div
+              class="rounded-full h-10 w-10 flex items-center justify-center bg-green-500 cursor-pointer"
+            >
+              <i class="text-4xl iconfont icon-plus"></i>
+            </div>
+          </div>
+          <!-- 会话列表 -->
+          <div></div>
+        </div>
+
+        <div class="flex-grow border">Right</div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ tethys-web/src/components/ChatPanel/_ChatHeader.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { reactive } from "vue";
+const state = reactive({
+  connected: false,
+});
+
+/**
+ * 连接 WebSocket.
+ */
+const connect = () => {
+  state.connected = true;
+};
+
+const disconnect = () => {
+  state.connected = false;
+};
+</script>
+<template>
+  <div class="flex gap-x-2">
+    <div class="flex-grow flex flex-col gap-2">
+      <div class="flex flex-row gap-x-2">
+        <div class="flex-grow">
+          <input type="text" placeholder="WebSocket URL" class="md:w-full" />
+        </div>
+        <div class="w-24">
+          <input type="text" placeholder="UID" class="md:w-full" />
+        </div>
+      </div>
+      <div>
+        <input type="text" placeholder="access_token" class="md:w-full" />
+      </div>
+    </div>
+    <div class="w-24 flex content-center">
+      <button
+        v-if="!state.connected"
+        class="md:w-full dark:text-white hover:text-red-500"
+        type="button"
+        @click="connect"
+      >
+        连接
+      </button>
+      <button
+        v-else
+        class="md:w-full dark:text-white hover:text-red-500"
+        type="button"
+        @click="disconnect"
+      >
+        断开链接
+      </button>
+    </div>
+  </div>
+</template>
