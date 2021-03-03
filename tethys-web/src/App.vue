129f9e6c2aa8ea982c09afffd478f7c70c672951
kk70 <kevinz@weghst.com>
2021-03-03 15:57:52 +0800

    完善界面布局

--- tethys-web/src/App.vue
+++ tethys-web/src/App.vue
@@ -1,26 +1,101 @@
 <template>
-  <button class="button is-primary">Dark</button>
-  <br />
-  <span class="icon-text">
-    <span class="icon">
-      <i class="iconfont icon-plus"></i>
-    </span>
-    <span>Home</span>
-  </span>
+  <div class="chat-container">
+    <div class="chat-inner">
+      <div class="chat-top px-4">这里是头部信息</div>
+      <div class="chat-left">这里是链接的参数配置信息</div>
+      <div class="chat-right">
+        <div class="chat-msg-body">聊天面板</div>
+        <div class="chat-action px-6">
+          <chat-action></chat-action>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import ChatAction from "./components/ChatAction.vue";
 
 export default defineComponent({
   name: "App",
+  components: { ChatAction },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import "./variables.css";
+
+.chat-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  max-height: 100vh;
+  overflow: hidden;
+  transition: 0.3s;
+}
+
+.chat-container .chat-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.chat-container .chat-top {
+  position: absolute;
+  top: 0;
+  left: var(--chat-left-width);
+  width: calc(100% - var(--chat-left-width));
+  height: var(--chat-top-height);
+  background: #191c26;
+  border-bottom: 1px solid #191c26;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 9;
+  transition: 0.3s;
+}
+
+.chat-container .chat-left {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: var(--chat-left-width);
+  height: 100%;
+  z-index: 10;
+  background: #191c26;
+  transition: 0.3s;
+}
+
+.chat-container .chat-right {
+  position: absolute;
+  top: var(--chat-top-height);
+  left: var(--chat-left-width);
+  height: 100%;
+  width: calc(100% - var(--chat-left-width));
+  background: #212532;
+  transition: 0.3s;
+}
+
+.chat-container .chat-msg-body {
+  position: relative;
+  width: 100%;
+  height: calc(100% - var(--chat-top-height) - var(--chat-action-height));
+  overflow-y: auto;
+  animation-name: fadeInLeft;
+  animation-duration: 0.5s;
+}
+
+.chat-container .chat-action {
+  position: fixed;
+  bottom: 8px;
+  left: var(--chat-left-width);
+  width: calc(100% - var(--chat-left-width));
+  height: var(--chat-action-height);
+  background: #212532;
+  z-index: 1;
+  transition: 0.3s;
 }
 </style>
---
+++ tethys-web/src/components/ChatAction.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="control">
+    <textarea
+      placeholder="在此对话中发送消息"
+      class="textarea p-2"
+      rows="2"
+    ></textarea>
+    <div class="toolbar">
+      <button class="button is-primary" style="height: 100%">发 送</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ChatAction",
+});
+</script>
+
+<style scoped>
+.control {
+  box-sizing: border-box;
+  clear: both;
+  font-size: 1rem;
+  position: relative;
+  text-align: inherit;
+}
+
+.control textarea {
+  background: #191c26;
+  border-color: #191c26;
+  color: #aeaeae;
+}
+
+textarea::placeholder {
+  color: #aeaeae;
+}
+
+.textarea {
+  resize: none;
+  height: 100%;
+  line-height: 2;
+  box-shadow: none !important;
+  transition: 0.3s;
+}
+
+.toolbar {
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+}
+</style>
