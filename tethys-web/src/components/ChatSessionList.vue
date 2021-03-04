9fcc85892d2343309d652053776e09b64bd97548
kk70 <kevinz@weghst.com>
2021-03-04 16:22:34 +0800

    更新样式

--- tethys-web/src/components/ChatSessionList.vue
+++ tethys-web/src/components/ChatSessionList.vue
@@ -1,17 +1,30 @@
 <template>
-  <div class="session-list">
-    <a class="session-item pl-2 is-active">
-      <figure class="image is-48x48">
-        <img class="is-rounded" src="https://via.placeholder.com/100?text=KK" />
-      </figure>
-      <div class="ml-2">我是KK</div>
-    </a>
-    <a class="session-item pl-2">
-      <figure class="image is-48x48">
-        <img class="is-rounded" src="https://via.placeholder.com/100?text=KK" />
-      </figure>
-      <div class="ml-2">我是KK</div>
-    </a>
+  <div class="inner-container">
+    <div class="session-list">
+      <a class="session-item pl-2 is-active">
+        <figure class="image is-48x48">
+          <img
+            class="is-rounded"
+            src="https://via.placeholder.com/100?text=KK"
+          />
+        </figure>
+        <div class="ml-2">我是KK</div>
+      </a>
+      <a class="session-item pl-2">
+        <figure class="image is-48x48">
+          <img
+            class="is-rounded"
+            src="https://via.placeholder.com/100?text=KK"
+          />
+        </figure>
+        <div class="ml-2">我是KK</div>
+      </a>
+    </div>
+    <div class="plus-container">
+      <button class="button plus-btn">
+        <i class="iconfont icon-plus has-text-white is-size-5"></i>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -23,8 +36,30 @@ export default defineComponent({
 </script>
 
 <style scoped>
+:inner-var {
+  --plus-container-height: 84px;
+}
+
+.inner-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: calc(100% - var(--chat-top-height));
+}
+
+.inner-container .plus-container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: var(--plus-container-height);
+  padding-bottom: 20px;
+}
+
 .session-list {
-  height: 100%;
+  width: 100%;
+  height: calc(100% - var(--chat-top-height) - var(--plus-container-height));
   overflow-y: auto;
 }
 
@@ -40,5 +75,13 @@ export default defineComponent({
 .session-item.is-active,
 .session-item:hover {
   background: var(--grey);
+}
+
+.plus-btn {
+  height: 48px;
+  width: 48px;
+  border: 3px dotted var(--grey-light);
+  border-radius: 50%;
+  background: var(--grey-lighter);
 }
 </style>
