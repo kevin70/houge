199117c7f73a94a317bd74008b3774878ec0c911
kk70 <kevinz@weghst.com>
2021-03-16 16:34:38 +0800

    在单元测试基础抽象类中新增 R2dbcClient 初始化

--- tethys-repository/src/test/java/top/yein/tethys/repository/AbstractTestRepository.java
+++ tethys-repository/src/test/java/top/yein/tethys/repository/AbstractTestRepository.java
@@ -1,8 +1,12 @@
 package top.yein.tethys.repository;
 
+import com.typesafe.config.ConfigFactory;
+import io.r2dbc.spi.ConnectionFactories;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.r2dbc.core.DatabaseClient;
@@ -17,6 +21,8 @@ import org.springframework.transaction.ReactiveTransactionManager;
 import org.springframework.transaction.reactive.TransactionalOperator;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import top.yein.tethys.core.r2dbc.DefaultR2dbcClient;
+import top.yein.tethys.r2dbc.R2dbcClient;
 
 /**
  * 测试存储的基类.
@@ -30,10 +36,21 @@ import reactor.core.publisher.Mono;
   DirtiesContextTestExecutionListener.class,
   TransactionalTestExecutionListener.class
 })
+@Log4j2
 public abstract class AbstractTestRepository {
 
   @Autowired protected DatabaseClient dc;
   @Autowired protected ReactiveTransactionManager transactionManager;
+
+  protected static R2dbcClient r2dbcClient;
+
+  @BeforeAll
+  static void setUp() {
+    var config = ConfigFactory.parseResources("tethys-test.conf");
+    log.debug("单元测试配置\n{}", config.root().render());
+    var connectionFactory = ConnectionFactories.get(config.getString("message-storage.r2dbc-url"));
+    r2dbcClient = new DefaultR2dbcClient(connectionFactory);
+  }
 
   /**
    * @param p
