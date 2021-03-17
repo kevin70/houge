9f6790a6eebe362a4727b6d03e8f841f035888b3
kk70 <kevinz@weghst.com>
2021-03-17 15:47:34 +0800

    更新完善

--- tethys-rest-server/src/main/java/top/yein/tethys/rest/module/RestModule.java
+++ tethys-rest-server/src/main/java/top/yein/tethys/rest/module/RestModule.java
@@ -7,9 +7,7 @@ import com.google.inject.multibindings.Multibinder;
 import top.yein.tethys.ApplicationIdentifier;
 import top.yein.tethys.core.http.Interceptors;
 import top.yein.tethys.core.http.RoutingService;
-import top.yein.tethys.core.id.YeinGidMessageIdGenerator;
 import top.yein.tethys.core.resource.AuthInterceptor;
-import top.yein.tethys.id.MessageIdGenerator;
 import top.yein.tethys.rest.RestApplicationIdentifier;
 import top.yein.tethys.rest.resource.MessageIdResource;
 
@@ -23,11 +21,11 @@ public class RestModule extends AbstractModule {
   @Override
   protected void configure() {
     bind(ApplicationIdentifier.class).to(RestApplicationIdentifier.class).in(Scopes.SINGLETON);
-    bind(MessageIdGenerator.class).to(YeinGidMessageIdGenerator.class).in(Scopes.SINGLETON);
-//    bind(TypeLiteral.get(RoutingService.class)).to(MessageIdResource.class).in(Scopes.SINGLETON);
 
     var routingServicesBinder = Multibinder.newSetBinder(binder(), RoutingService.class);
     routingServicesBinder.addBinding().to(MessageIdResource.class).in(Scopes.SINGLETON);
+//    routingServicesBinder.addBinding().to(PrivateMessageResource.class).in(Scopes.SINGLETON);
+//    routingServicesBinder.addBinding().to(GroupMessageResource.class).in(Scopes.SINGLETON);
   }
 
   @Provides
