8580f8a925191f32d9c1995fec31e2b9663f6d6a
kk70 <kevinz@weghst.com>
2021-03-19 11:27:32 +0800

    修改 groups 数据库表结构

--- db/migrations/V1.0.0.9__create_groups.sql
+++ db/migrations/V1.0.0.9__create_groups.sql
@@ -11,6 +11,7 @@ create table groups
     creator_id  bigint    not null,
     owner_id    bigint,
     member_size integer   not null,
+    member_limit integer   not null,
     create_time timestamp not null,
     update_time timestamp not null
 );
@@ -26,6 +27,8 @@ comment on column groups.creator_id is '创建者用户 ID';
 comment on column groups.owner_id is '拥有者用户 ID';
 
 comment on column groups.member_size is '群成员数量';
+
+comment on column groups.member_limit is '群成员数量限制';
 
 comment on column groups.create_time is '创建时间';
 
