fd7288028c3dd1d6bfa120e51ce2078fef822e02
kk70 <kevinz@weghst.com>
2021-03-22 14:22:03 +0800

    私聊消息校验接收方是否存在

--- tethys-im-server/src/main/java/top/yein/tethys/im/handler/PrivateMessageHandler.java
+++ tethys-im-server/src/main/java/top/yein/tethys/im/handler/PrivateMessageHandler.java
@@ -3,17 +3,20 @@ package top.yein.tethys.im.handler;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import lombok.extern.log4j.Log4j2;
 import reactor.core.publisher.Mono;
 import top.yein.chaos.biz.BizCodeException;
+import top.yein.tethys.Null;
 import top.yein.tethys.core.BizCodes;
 import top.yein.tethys.core.MessageProperties;
 import top.yein.tethys.entity.Message;
 import top.yein.tethys.id.MessageIdGenerator;
 import top.yein.tethys.im.server.PacketHandler;
 import top.yein.tethys.packet.PrivateMessagePacket;
+import top.yein.tethys.service.UserService;
 import top.yein.tethys.session.Session;
 import top.yein.tethys.session.SessionManager;
 import top.yein.tethys.storage.MessageDao;
@@ -30,6 +33,7 @@ public class PrivateMessageHandler implements PacketHandler<PrivateMessagePacket
   private final MessageDao messageDao;
   private final MessageProperties messageProperties;
   private final MessageIdGenerator messageIdGenerator;
+  private final UserService userService;
 
   /**
    * 构造函数.
@@ -38,17 +42,20 @@ public class PrivateMessageHandler implements PacketHandler<PrivateMessagePacket
    * @param messageDao
    * @param messageProperties 聊天消息静态配置
    * @param messageIdGenerator 消息 ID 生成器
+   * @param userService
    */
   @Inject
   public PrivateMessageHandler(
       SessionManager sessionManager,
       MessageDao messageDao,
       MessageProperties messageProperties,
-      MessageIdGenerator messageIdGenerator) {
+      MessageIdGenerator messageIdGenerator,
+      UserService userService) {
     this.sessionManager = sessionManager;
     this.messageDao = messageDao;
     this.messageProperties = messageProperties;
     this.messageIdGenerator = messageIdGenerator;
+    this.userService = userService;
   }
 
   @Override
@@ -65,28 +72,42 @@ public class PrivateMessageHandler implements PacketHandler<PrivateMessagePacket
 
     // 校验消息
     // FIXME
-//    MessagePacketChecker.check(packet);
+    //    MessagePacketChecker.check(packet);
     var from = Optional.ofNullable(packet.getFrom()).orElseGet(session::uid);
     var to = packet.getTo();
 
-    // FIXME 校验 to 用户是否存在
+    // 路由、存储消息
+    Function<Null, Mono<Void>> bizFun =
+        (unused) -> {
+          var p1 =
+              sessionManager.findByUid(to).delayUntil(toSession -> toSession.sendPacket(packet));
 
-    var p1 = sessionManager.findByUid(to).delayUntil(toSession -> toSession.sendPacket(packet));
+          // 存储的消息实体
+          var entity =
+              Message.builder()
+                  .id(packet.getMessageId())
+                  .senderId(from)
+                  .receiverId(to)
+                  .kind(Message.KIND_PRIVATE)
+                  .content(packet.getContent())
+                  .contentKind(packet.getContentKind())
+                  .url(packet.getUrl())
+                  .customArgs(packet.getCustomArgs())
+                  .unread(Message.MESSAGE_UNREAD)
+                  .build();
+          var p2 = messageDao.insert(entity, List.of(from, to));
+          return p2.thenMany(p1).then();
+        };
 
-    // 存储的消息实体
-    var entity =
-        Message.builder()
-            .id(packet.getMessageId())
-            .senderId(from)
-            .receiverId(to)
-            .kind(Message.KIND_PRIVATE)
-            .content(packet.getContent())
-            .contentKind(packet.getContentKind())
-            .url(packet.getUrl())
-            .customArgs(packet.getCustomArgs())
-            .unread(Message.MESSAGE_UNREAD)
-            .build();
-    var p2 = messageDao.insert(entity, List.of(from, to));
-    return p2.thenMany(p1).then();
+    return userService
+        .existsById(to)
+        .switchIfEmpty(
+            Mono.error(
+                () -> {
+                  var e = new BizCodeException(BizCodes.C3630).addContextValue("to", to);
+                  log.warn("用户[{}]发送私聊消息给不存在的用户[{}] {}", from, to, packet);
+                  return e;
+                }))
+        .flatMap(bizFun);
   }
 }
