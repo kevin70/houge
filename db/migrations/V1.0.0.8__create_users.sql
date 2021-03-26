a868b713151bb67d2425ce70a2060f16f6fb9d4e
kk70 <kevinz@weghst.com>
2021-03-26 14:38:35 +0800

    去除 users.origin_uid 列的约束

--- db/migrations/V1.0.0.8__create_users.sql
+++ db/migrations/V1.0.0.8__create_users.sql
@@ -1,11 +1,11 @@
 create table users
 (
-    id          bigint       not null
+    id          bigint    not null
         constraint users_pk
             primary key,
-    origin_uid  varchar(128) not null,
-    create_time timestamp    not null,
-    update_time timestamp    not null
+    origin_uid  varchar(128),
+    create_time timestamp not null,
+    update_time timestamp not null
 );
 
 comment on column users.origin_uid is '原用户 ID';
@@ -13,3 +13,6 @@ comment on column users.origin_uid is '原用户 ID';
 comment on column users.create_time is '创建时间';
 
 comment on column users.update_time is '更新时间';
+
+create unique index users_origin_uid_uindex
+    on users (origin_uid);
