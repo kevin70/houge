b2d95a749e4528edd9ddf13e4c554b6676001c43
kk70 <kevinz@weghst.com>
2021-04-12 16:36:04 +0800

    新增注释

--- tethys-grpc-service/src/main/java/top/yein/tethys/grpc/service/SingleRequestMessagePacket.java
+++ tethys-grpc-service/src/main/java/top/yein/tethys/grpc/service/SingleRequestMessagePacket.java
@@ -1,10 +1,29 @@
+/*
+ * Copyright 2019-2021 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package top.yein.tethys.grpc.service;
 
 import top.yein.tethys.grpc.MessageRequest;
 import top.yein.tethys.packet.MessagePacket;
 import top.yein.tethys.packet.Namespaces;
 
-/** @author KK (kzou227@qq.com) */
+/**
+ * gRPC 单消息发送请求.
+ *
+ * @author KK (kzou227@qq.com)
+ */
 class SingleRequestMessagePacket implements MessagePacket {
 
   private final String messageId;
