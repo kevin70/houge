c1925cb457ff9829761782b77197502e6f30c2a5
kk70 <kevinz@weghst.com>
2021-06-01 15:30:45 +0800

    自动生成的代码

---
+++ houge-server-rest/src/main/generated/cool/houge/rest/controller/group/GroupMapperImpl.java
@@ -0,0 +1,29 @@
+package cool.houge.rest.controller.group;
+
+import cool.houge.service.group.JoinMemberInput;
+import cool.houge.service.group.JoinMemberInput.JoinMemberInputBuilder;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-05-31T13:49:12+0800",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
+)
+public class GroupMapperImpl implements GroupMapper {
+
+    @Override
+    public JoinMemberInput map(JoinMemberBody vo, long gid) {
+        if ( vo == null ) {
+            return null;
+        }
+
+        JoinMemberInputBuilder joinMemberInput = JoinMemberInput.builder();
+
+        if ( vo != null ) {
+            joinMemberInput.uid( vo.getUid() );
+        }
+        joinMemberInput.gid( gid );
+
+        return joinMemberInput.build();
+    }
+}
